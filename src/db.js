66fe496a6b969831b4fc90836497559f675f0743
tirmo05 <tirsomorato@gmail.com>
2026-02-28 18:04:07 +0100

    db

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ import pg from 'pg'
 const { Pool } = pg
 
 export const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
     port: 5432,
