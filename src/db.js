f78a57afbdf099e2c7bda0878121f9bcdd163a99
tirmo05 <tirsomorato@gmail.com>
2026-02-28 13:56:25 +0100

    feat: Initialize PostgreSQL database connection pool using environment variables and SSL.

--- src/db.js
+++ src/db.js
@@ -3,9 +3,10 @@ import pg from 'pg'
 const { Pool } = pg
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
 })
