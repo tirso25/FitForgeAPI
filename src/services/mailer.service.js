6fc0b4a35e9f0656fec322cf25262aea79c33107
tirmo05 <tirsomorato@gmail.com>
2026-02-28 23:03:58 +0100

    feat: add mailer service to configure Nodemailer for sending emails via Gmail.

--- src/services/mailer.service.js
+++ src/services/mailer.service.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_APP_PASSWORD,
+        pass: process.env.GMAIL_APP_PASSWORD ? process.env.GMAIL_APP_PASSWORD.replace(/\s+/g, '') : '',
     },
 });
 
